7e9ea4fdeff5fda291d6874a582c46ffdb253e60
daryn <daryn@hex50.com>
2020-07-02 14:20:12 -0700

    done

--- form/static/js/main.js
+++ form/static/js/main.js
@@ -1,3 +1,7 @@
+// added CSSRule GSAP plugin - see js scripts in base.html
+// we can now target the span:after CSS property:
+var rule = CSSRulePlugin.getRule('span:after');
+
 
 // starts at opacity 0 (invisible) and over 'duration' the 
 // div block transitions opacity to 1 and moves in the 
@@ -11,11 +15,22 @@
 //      Power2.easeInOut
 //      elastic(1,0.3)
 //gsap.from('div', {opacity: 0, duration: 1, y: -50, ease: 'elastic(1,0.3)'})
-
-gsap.from('div', {opacity: 0, duration: 1, y: -50})
+// [v1]
+// gsap.from('div', {opacity: 0, duration: 1, y: -50});
+// [v2] - target class 'anim1', add stagger - staggers the
+// animation of all 'anim1' class elements (Very cool)
+gsap.from('.anim1', {opacity: 0, duration: 1, y: -50, stagger: 0.6});
 
 // target the img, opposite y movement:
-gsap.from('img', {opacity: 0, duration: 1, y: 30})
+// [v1]
+// gsap.from('img', {opacity: 0, duration: 1, y: 30})
+// [v2] - add delay
+gsap.from('img', {opacity: 0, duration: 1, y: 30, delay: 1.4})
+
+// added to fire the 'CSSRule above'
+// each span in the h1 is revealed on the Y axis simultaneously
+// very cool!
+gsap.to(rule,{cssRule: {scaleY: 0}, duration: 1})
 
 // target aside element, this shows a scroll bar until
 // duration is complete (Ew!)
@@ -27,8 +42,11 @@ gsap.from('img', {opacity: 0, duration: 1, y: 30})
 // we want to animate the background 'position' not x or y.
 // so we use the CSS property 'background-position' but in GSAP
 // we camel case the CSS property; 'backgroundPosition'
-gsap.from('aside', {opacity: 0, duration: 1, backgroundPosition: '200px 0px'})
+// [v1]
+// gsap.from('aside', {opacity: 0, duration: 1, backgroundPosition: '200px 0px'})
 
+// [v2] - add delay
+gsap.from('aside', {opacity: 0, duration: 1, backgroundPosition: '200px 0px', delay: 1.1})
 
 
 
---
+++ form/static/js/main2.js
@@ -0,0 +1,16 @@
+// the 'timeline' way...
+
+var rule = CSSRulePlugin.getRule('span:after');
+
+// setup common properties, i.e. duration:
+var tl = gsap.timeline({defaults: {duration: 1}})
+
+tl.from(".anim1", {opacity: 0, y: -50, stagger: 0.6})
+    .to(rule, {duration: 1.8, cssRule: {scaleY: 0}}, "-=2.2")
+    .from('aside', {opacity: 0, backgroundPosition: '200px 0px'}, "-=1.5")
+    .from('img', {opacity: 0, y: 50}, "-=0.5");  
+
+// can reverse the entire timeline...
+document.getElementById('cta').addEventListener('click', () => {
+    tl.reversed() ? tl.play() : tl.reverse()
+})
